d616ffe3f8876f34dea594d966cf02f04b5b5ed1
alex123 <alex123@dot.com>
2024-04-17 12:10:49 +0500

    Start

---
+++ src/main/java/ru/gb/group4546_5984/spring_sem06_hw_task02/SpringSem06HwTask02Application.java
@@ -0,0 +1,13 @@
+package ru.gb.group4546_5984.spring_sem06_hw_task02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSem06HwTask02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSem06HwTask02Application.class, args);
+	}
+
+}
